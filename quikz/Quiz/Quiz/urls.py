e49746d29e8405ab4816042030027640f8ff4cb5
meduzah <129514536+meduzah@users.noreply.github.com>
2024-04-17 14:38:05 +0200

    naprawa redirectview
    w urls.py zmienilam redirect views

--- quikz/Quiz/Quiz/urls.py
+++ quikz/Quiz/Quiz/urls.py
@@ -1,11 +1,14 @@
-from django.contrib import admin  
-from django.urls import path, include  
-from django.conf import settings  
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns  
-from django.contrib.staticfiles.urls import static  
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import static
+from django.views.generic import RedirectView
 
-urlpatterns = [  
- path('admin/', admin.site.urls),  
- path('', include("App.urls")),  
+urlpatterns = [
+ path('admin/', admin.site.urls),
+ path('', include("App.urls")),
+ path('catalog/', RedirectView.as_view(url='/login')),
+ path('cats/', RedirectView.as_view(url='/login')),
 
-]  
+]
